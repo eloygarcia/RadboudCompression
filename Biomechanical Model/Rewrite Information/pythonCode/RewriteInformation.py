8787070a6f955f87261fe2a2819d0986fdc8cab0
eloygarcia <eloygm82@gmail.com>
2023-11-13 17:07:36 +0100

    Working with large files without memory (first try)

---
+++ Biomechanical Model/Rewrite Information/pythonCode/RewriteInformation.py
@@ -0,0 +1,24 @@
+## Hi Eloy!
+# Fist of all:
+# - check swap memory
+# - Release swap memory
+# - run from terminal
+#
+#
+# TO DO:
+# - Write a main file to directly use the terminal, including input and output filenames
+
+import SimpleITK as sitk
+# import pylab
+import imageio.v3 as iio
+import numpy as np
+
+####
+inputfilename = '/home/eloygarcia/Escritorio/UCM/models/hetero_50um.tif'
+outputfilename = '/home/eloygarcia/Escritorio/UCM/models/hetero_50um.nrrd'
+
+#### SCATTERED
+# inputfilename = '/home/eloygarcia/Escritorio/UCM/models/scattered_50um.tif'
+# outputfilename = '/home/eloygarcia/Escritorio/UCM/models/scattered_50um.nrrd'
+
+sitk.WriteImage( sitk.GetImageFromArray(iio.imread(inputfilename)), outputfilename, useCompression=True)
